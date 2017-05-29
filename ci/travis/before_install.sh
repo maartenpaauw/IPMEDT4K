26ad0430ba66eacdfd94e7898c0fbda22ba116a5
Maarten Paauw <maartenpaauw@gmail.com>
2017-05-29 17:29:17 +0200

    Travis Ci

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -2,3 +2,5 @@
 
 mysql -e 'CREATE DATABASE IPMEDT4K;'
 composer self-update
+composer install --no-interaction
+composer dump-autoload -o
--- ci/travis/before_script.sh
+++ ci/travis/before_script.sh
@@ -3,6 +3,6 @@
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 ./vendor/laravel/dusk/bin/chromedriver-linux &
-composer install --no-interaction
 cp .env.travis .env
+chmod -R 0755 vendor/laravel/dusk/bin
 chmod 777 -R storage
