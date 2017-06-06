ebd0e1af015eb23fff262c3489aedef916e81b16
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-06 22:21:28 +0200

    (Vue) weather and news

---
+++ resources/assets/js/components/monitor/News.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="row my-5 px-1 py-3 white-bg border-light-gray">
+        <div class="col-1">
+            <img :src="logo" alt="Nu.nl - Logo" style="height: 50px;">
+        </div>
+        <div class="col align-self-center">
+            <div class="news">
+                <p class="m-0 mr-5 news-item" v-for="item in news">
+                    <strong class="text-info">{{ item.date | date }}</strong><span class="dark-blue">&emsp;&mdash;&emsp;</span>{{ item.title }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['logo'],
+        data () {
+            return {
+                news: []
+            }
+        },
+        methods: {
+            getNews () {
+                axios.get('/api/nu.nl')
+                    .then( (response) => {
+                        this.news = response.data;
+                    })
+                ;
+            }
+        },
+        created () {
+            this.getNews();
+            setInterval(() => {
+                this.getNews();
+            }, 1000 * 60 * 5);
+        },
+        filters: {
+            date (value) {
+                return moment(value).format('HH:mm');
+            }
+        }
+    }
+</script>
--- resources/views/monitor/monitor.blade.php
+++ resources/views/monitor/monitor.blade.php
@@ -55,84 +55,13 @@
                             <monitor-triage-counter></monitor-triage-counter>
 
                             <!-- Weather -->
-                            <div class="col-12 p-4 mb-5 white-bg border-light-gray">
-                                <div class="row">
-
-                                    <div class="col-12">
-                                        <h3 class="h4 dark-blue">
-                                            <strong>Leiden,</strong> Nederland
-                                        </h3>
-                                        <small>
-                                            <i class="text-info">Hoogte 0m 52.15 N, 4.49 E&emsp;<span class="blue-dark">|</span>&emsp;Geüpdatet 12 minuten geleden</i>
-                                        </small>
-                                    </div>
-
-                                    <div class="col-12 mt-5 mb-4">
-                                        <div class="row">
-                                            <div class="col">
-                                                <p class="h1 text-center" style="font-size: 4rem;">
-                                                    <i class="wi wi-day-rain mr-3 text-info"></i> <span class="dark-blue">13.5<sup>&deg;c</sup></span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="col-12 my-2">
-                                        <hr>
-                                    </div>
-
-                                    <div class="col-12">
-                                        <div class="row">
-                                            <div class="col">
-                                                <p class="m-0 text-info">
-                                                    <i class="wi wi-sunrise fa-2x  m-0 mr-2 align-middle"></i> <span class="align-middle blue-dark">06:54</span>
-                                                </p>
-                                            </div>
-                                            <div class="col">
-                                                <p class="m-0 text-info">
-                                                    <i class="wi wi-sunset  fa-2x m-0 mr-2 align-middle"></i> <span class="align-middle blue-dark">20:31</span>
-                                                </p>
-                                            </div>
-                                            <div class="col-5">
-                                                <p class="m-0 text-info text-right">
-                                                    <i class="wi wi-strong-wind fa-2x m-0 mr-2 align-middle"></i> <span class="align-middle blue-dark">6.1 km/h - ZW</span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <monitor-weather></monitor-weather>
                         </div>
                     </div>
                 </div>
 
                 <!-- NU.nl -->
-                <div class="row my-5 px-1 py-3 white-bg border-light-gray">
-
-                    <!-- Logo -->
-                    <div class="col-1">
-                        <img src="{!! asset('images/nu.nl.png') !!}" alt="Nu.nl - Logo" style="height: 50px;">
-                    </div>
-
-                    <!-- Feed -->
-                    <div class="col align-self-center">
-
-                        <!-- News -->
-                        <div class="news">
-
-                            <!-- News item -->
-                            <p class="m-0 mr-5 news-item">
-                                <strong class="text-info">06:45</strong><span class="dark-blue">&emsp;&mdash;&emsp;</span>Nieuwe advocaat voor verdachte in zaak Everink.
-                            </p>
-
-                            <!-- News item -->
-                            <p class="m-0 news-item">
-                                <strong class="text-info">06:45</strong><span class="dark-blue">&emsp;&mdash;&emsp;</span>Nieuwe advocaat voor verdachte in zaak Everink.
-                            </p>
-                        </div>
-
-                    </div>
-                </div>
+                <monitor-news logo="{!! asset('images/nu.nl.png') !!}"></monitor-news>
             </div>
 
         </div>
