5ab8bc38093a4ace55f6333f3c17524050664506
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-13 11:26:52 +0200

    Triage scopes

--- app/Models/Triage.php
+++ app/Models/Triage.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Triage extends Model 
 {
+    public function scopeOnmiddelijk ($query) {
+        return $query->where('slug', str_slug('Onmiddelijk'))->first();
+    }
 
+    public function scopeHoogUrgent ($query) {
+        return $query->where('slug', str_slug('Hoog urgent'))->first();
+    }
+
+    public function scopeUrgent ($query) {
+        return $query->where('slug', str_slug('Urgent'))->first();
+    }
+
+    public function scopeStandaard ($query) {
+        return $query->where('slug', str_slug('Standaard'))->first();
+    }
+
+    public function scopeNietUrgent ($query) {
+        return $query->where('slug', str_slug('Niet urgent'))->first();
+    }
 }
