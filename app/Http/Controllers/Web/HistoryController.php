7a96f3c20e058e41d9366456df89a4dac67c0a35
Zowie van Geest <zowie@zowievangeest.nl>
2017-06-13 23:53:11 +0200

    Added destroy in history controller

--- app/Http/Controllers/Web/HistoryController.php
+++ app/Http/Controllers/Web/HistoryController.php
@@ -4,8 +4,23 @@ namespace IPMEDT4K\Http\Controllers\Web;
 
 use Illuminate\Http\Request;
 use IPMEDT4K\Http\Controllers\Controller;
+use IPMEDT4K\Models\Patient;
 
 class HistoryController extends Controller
 {
-    //
+    public function index()
+    {
+        $patienten = Patient::all();
+
+        return view('dashboard.historie.index')
+            ->with('patienten', $patienten
+            );
+    }
+
+    public function destroy($id) {
+        $patient = Patient::findOrFail($id);
+        $patient->delete();
+
+        return redirect('historie');
+    }
 }
