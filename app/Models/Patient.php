360cdb2fec2e19e0f43c818dca973312cdf09068
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-13 11:31:12 +0200

    Patient triage scopes

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -15,4 +15,24 @@ class Patient extends Model
     public function status () {
         return $this->hasOne(Status::class, 'id', 'status_id');
     }
+
+    public function scopeOnmiddelijk ($query) {
+        return $query->where('triage_id', Triage::onmiddelijk()->id);
+    }
+
+    public function scopeHoogUrgent ($query) {
+        return $query->where('triage_id', Triage::hoogUrgent()->id);
+    }
+
+    public function scopeUrgent ($query) {
+        return $query->where('triage_id', Triage::Urgent()->id);
+    }
+
+    public function scopeStandaard ($query) {
+        return $query->where('triage_id', Triage::standaard()->id);
+    }
+
+    public function scopeNietUrgent ($query) {
+        return $query->where('triage_id', Triage::nietUrgent()->id);
+    }
 }
