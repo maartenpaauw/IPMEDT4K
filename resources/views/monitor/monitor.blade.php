96d00e283e9bc4e033583965f69e9ae0b2c029ea
Maarten Paauw <maartenpaauw@gmail.com>
2017-06-27 09:54:00 +0200

    Favicon

--- resources/views/monitor/monitor.blade.php
+++ resources/views/monitor/monitor.blade.php
@@ -9,6 +9,24 @@
 
         <title>Krankenhaus Krak&emsp;&mdash;&emsp;Monitor</title>
 
+        <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('favicon/apple-icon-57x57.png') }}">
+        <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('favicon/apple-icon-60x60.png') }}">
+        <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('favicon/apple-icon-72x72.png') }}">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('favicon/apple-icon-76x76.png') }}">
+        <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('favicon/apple-icon-114x114.png') }}">
+        <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('favicon/apple-icon-120x120.png') }}">
+        <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('favicon/apple-icon-144x144.png') }}">
+        <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('favicon/apple-icon-152x152.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-icon-180x180.png') }}">
+        <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('favicon/android-icon-192x192.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicon/favicon-96x96.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('favicon/manifest.json') }}">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="{{ asset('faviconms-icon-144x144.png') }}">
+        <meta name="theme-color" content="#ffffff">
+
         <meta name="csrf-token" content="{!! csrf_token() !!}">
     </head>
 
@@ -21,7 +39,7 @@
             <monitor-header logo="{!! asset('images/logo.png') !!}"></monitor-header>
 
             <!-- Container -->
-            <div class="container">
+            <div class="container-fluid mx-5">
 
                 <!-- Main -->
                 <div class="row pt-5">
@@ -49,7 +67,7 @@
                             <monitor-weather></monitor-weather>
 
                             <!-- Go to site bar -->
-                            <monitor-site></monitor-site>
+                            <monitor-site url="{{ route('patient.login') }}"></monitor-site>
                         </div>
                     </div>
                 </div>
