4ed60599a09c480bc50da352bf147b9620e93662
Zowie van Geest <zowie@zowievangeest.nl>
2017-06-30 17:36:40 +0200

    Added model path

--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => IPMEDT4K\User::class,
+        'model' => IPMEDT4K\Models\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
